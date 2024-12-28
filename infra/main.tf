76233a7218d88722f11b24338640c3e8c0523a14
czarekemce <c.szymanski@interia.eu>
2024-12-29 00:03:50 +0100

    Add lambda function

--- infra/main.tf
+++ infra/main.tf
@@ -23,6 +23,8 @@ resource "aws_instance" "playwright_instance" {
     npx playwright install
     npx playwright install-deps
 
+    sudo pip install boto3
+    
     sudo apt install -y cron
     sudo systemctl enable cron
     sudo systemctl start cron
@@ -86,12 +88,15 @@ resource "aws_iam_policy" "s3_access_policy" {
           "Effect": "Allow",
           "Action": [
             "s3:PutObject",
+            "s3:PutObjectAcl",
             "s3:GetObject",
-            "s3:ListMultipartUploadParts",
-            "s3:AbortMultipartUpload"
+            "s3:GetObjectAcl",
+            "s3:DeleteObject"
           ],
           "Resource": [
-            "arn:aws:s3:::/*"
+            "arn:aws:s3:::${var.bucketname}/*",
+            "arn:aws:s3:::${var.bucketname}"
+
           ]
         }
       ]
@@ -107,4 +112,95 @@ resource "aws_iam_role_policy_attachment" "ec2_role_attachment" {
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2InstanceProfile"
   role = aws_iam_role.ec2_role.name
+}
+
+# Rola IAM dla funkcji Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
+}
+
+# Polityka IAM dla Lambda (dostęp do S3 i SNS)
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda-policy"
+  description = "Policy for Lambda to access S3 and SNS"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject"
+        ]
+        Resource = "arn:aws:s3:::${aws_s3_bucket.price_file.bucket}/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "sns:Publish"
+        ]
+        Resource = aws_sns_topic.price_alerts.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+# Funkcja Lambda
+resource "aws_lambda_function" "price_check_lambda" {
+  filename         = "lambda.zip"
+  function_name    = "PriceCheckLambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+# Powiadomienie S3, aby uruchomić funkcję Lambda po załadowaniu pliku
+resource "aws_s3_bucket_notification" "price_file_notification" {
+  bucket = aws_s3_bucket.price_file.id
+
+  lambda_function {
+    events     = ["s3:ObjectCreated:*"]
+    lambda_function_arn = aws_lambda_function.price_check_lambda.arn
+  }
+}
+
+# Uprawnienia dla S3, aby mogło wywoływać funkcję Lambda
+resource "aws_lambda_permission" "allow_s3_trigger" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.price_check_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.price_file.arn
+}
+
+# Zasób SNS - temat powiadomień
+resource "aws_sns_topic" "price_alerts" {
+  name = "price-alerts"
+}
+
+# Subskrypcja SNS - powiadomienie email
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.price_alerts.arn
+  protocol  = "email"
+  endpoint  = "cezary_pocztax@wp.pl" 
 }
