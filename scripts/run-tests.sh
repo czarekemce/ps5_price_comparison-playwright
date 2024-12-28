34ab12241a4deb864088fd34a4ff6fced61be89f
czarekemce <c.szymanski@interia.eu>
2024-12-28 20:04:00 +0100

    Fix bash script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /app/tests
+cd /app
 npx playwright test
 
 source /app/config.txt
@@ -8,7 +8,7 @@ echo $bucketname
 
 if [ $? -eq 0 ]; then
     echo "Tests passed. Uploading prices.txt to S3..."
-    aws s3 cp /app/prices.txt s3://$bucketname/
+    aws s3 cp /app/prices.txt s3://$bucketname/prices.txt
 else
     echo "Tests failed. Skipping S3 upload."
     exit 1
