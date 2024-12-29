b9fee97c3a9bed615c8a52ac03f5702184d917f4
czarekemce <c.szymanski@interia.eu>
2024-12-29 16:05:22 +0100

    Add company name in lambda function

--- infra/lambda_function.py
+++ infra/lambda_function.py
@@ -20,7 +20,7 @@ def lambda_handler(event, context):
         try:
             for line in file_content.splitlines():
                 replaced = re.sub(r'\s+', '', line.strip())
-                splitted = replaced.replace('zł', '')
+                company = replaced[4:]
                 value = int(replaced[:4])
                 if value <= 2000:
                     alert_triggered = True
@@ -30,7 +30,7 @@ def lambda_handler(event, context):
 
         if alert_triggered:
             message = {
-                "Message": f"Cena PS5 spadła poniżej 2000! {value} in {splitted[4:]}",
+                "Message": f"Cena PS5 spadła poniżej 2000! {value} in {company}",
                 "Subject": "Alert - Kwota poniżej 2000",
                 "TopicArn": "arn:aws:sns:eu-central-1:872515283157:price-alerts"
             }
